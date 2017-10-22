90ab753061025dcdfcb73372ed4b189f8e29c620
Bradford Knowlton <brad@ucanbonline.net>
2017-10-22 13:35:56 -0600

    Saving progress, not tested code

---
+++ index.js
@@ -0,0 +1,52 @@
+/*!
+ * SQLCacheDB 
+ *
+ * Copyright(c) 2017 Bradford Knowlton
+ * MIT Licensed
+ */
+
+'use strict'
+
+var sqlite3 = require('sqlite3');
+var cacheDb = new sqlite3.Database('./data/database.db');
+
+function getCache(key){
+
+	var query = cacheDb.prepare("SELECT * FROM `cache` WHERE `key` = '(?)'", key);
+
+	cacheDb.get(query, function(err, row) {
+
+		if ( err || row == undefined ){
+			// cache miss
+			console.log('Cache MISS'); 
+			
+			return null;
+		}else{
+			// cache hit
+			console.log('Cache HIT');
+
+			var data = row.data;
+
+			return data;
+		}
+
+	});
+
+	cacheDb.close();
+
+}
+
+function putCache(key, data){
+
+	cacheDb.serialize(function() { 
+		
+		var stmt = cacheDb.prepare("INSERT OR REPLACE INTO `cache`(`ID`,`key`,`data`,`date_created`) VALUES ( NULL, (?), (?), datetime('now') );");
+
+		stmt.run( key, data );
+        
+      	stmt.finalize();
+
+	});
+
+	cacheDb.close();
+}
