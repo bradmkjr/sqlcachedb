9b2fd4da904a8b09d1e3e927a48253dd2972cd16
Bradford Knowlton <brad@ucanbonline.net>
2017-10-22 14:19:21 -0600

    Installed ESLint dev, clean code!

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
  * Copyright(c) 2017 Bradford Knowlton
  * MIT Licensed
  *
- * Version 1.0.4
+ * Version 1.0.5
  */
 
-'use strict'
+'use strict';
 
 var sqlite3 = require('sqlite3');
 var cacheDb = new sqlite3.Database('./data/database.db');
@@ -15,35 +15,26 @@ var cacheDb = new sqlite3.Database('./data/database.db');
 exports.sqlCacheDb = function(){
 
 	cacheDb.serialize(function() {
-	  cacheDb.run('CREATE TABLE "cache" (
-		`ID`	INTEGER PRIMARY KEY AUTOINCREMENT,
-		`key`	TEXT UNIQUE,
-		`data`	TEXT,
-		`date_created`	TEXT,
-		`date_updated`	TEXT
-	);');
-	 
+		cacheDb.run('CREATE TABLE "cache" ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `key` TEXT UNIQUE, `data` TEXT, `date_created` TEXT, `date_updated` TEXT	);');	 
 	});
-	 
+
 	cacheDb.close();
 
-}
+};
 
 exports.getCache = function(key){
 
-	var query = cacheDb.prepare("SELECT * FROM `cache` WHERE `key` = '(?)'", key);
+	var query = cacheDb.prepare('SELECT * FROM `cache` WHERE `key` = "(?)"', key);
 
 	cacheDb.get(query, function(err, row) {
 
 		if ( err || row == undefined ){
 			// cache miss
-			console.log('Cache MISS'); 
 			
 			return null;
 		}else{
 			// cache hit
-			console.log('Cache HIT');
-
+			
 			var data = row.data;
 
 			return data;
@@ -53,34 +44,34 @@ exports.getCache = function(key){
 
 	cacheDb.close();
 
-}
+};
 
 exports.putCache = function(key, data){
 
 	cacheDb.serialize(function() { 
 		
-		var stmt = cacheDb.prepare("INSERT OR REPLACE INTO `cache`(`ID`,`key`,`data`,`date_created`) VALUES ( NULL, (?), (?), datetime('now') );");
+		var stmt = cacheDb.prepare('INSERT OR REPLACE INTO `cache`(`ID`,`key`,`data`,`date_created`) VALUES ( NULL, (?), (?), datetime("now") );');
 
 		stmt.run( key, data );
-        
-      	stmt.finalize();
+
+		stmt.finalize();
 
 	});
 
 	cacheDb.close();
-}
+};
 
 exports.purgeCache = function(key, data){
 
 	cacheDb.serialize(function() { 
 		
-		var stmt = cacheDb.prepare("");
+		var stmt = cacheDb.prepare('');
 
 		stmt.run( key, data );
-        
-      	stmt.finalize();
+
+		stmt.finalize();
 
 	});
 
 	cacheDb.close();
-}
+};
