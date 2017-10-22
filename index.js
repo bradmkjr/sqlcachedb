d2d50ea2cab0464c81a46faa0e16fb9f1971a412
Bradford Knowlton <brad@ucanbonline.net>
2017-10-22 14:35:09 -0600

    Added getKeys and getActiveKeys

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  * Copyright(c) 2017 Bradford Knowlton
  * MIT Licensed
  *
- * Version 1.0.5
+ * Version 1.0.6
  */
 
 'use strict';
@@ -12,8 +12,10 @@
 var sqlite3 = require('sqlite3');
 var cacheDb = new sqlite3.Database('./data/database.db');
 
-exports.sqlCacheDb = function(){
+// var cacheLifetime = '-6 hours';
 
+exports.sqlCacheDb = function(){
+	// Create the database table if it doesn't exist
 	cacheDb.serialize(function() {
 		cacheDb.run('CREATE TABLE "cache" ( `ID` INTEGER PRIMARY KEY AUTOINCREMENT, `key` TEXT UNIQUE, `data` TEXT, `date_created` TEXT, `date_updated` TEXT	);');	 
 	});
@@ -23,7 +25,7 @@ exports.sqlCacheDb = function(){
 };
 
 exports.getCache = function(key){
-
+	// query database for data based on key with date within lifetime
 	var query = cacheDb.prepare('SELECT * FROM `cache` WHERE `key` = "(?)"', key);
 
 	cacheDb.get(query, function(err, row) {
@@ -34,16 +36,48 @@ exports.getCache = function(key){
 			return null;
 		}else{
 			// cache hit
-			
 			var data = row.data;
 
 			return data;
 		}
-
 	});
+	cacheDb.close();
+};
 
+exports.getKeys = function(){
+	// query database for all keys
+	var query = cacheDb.prepare('SELECT keys FROM `cache`');
+
+	var keys = [];
+
+	cacheDb.each(query, function(err, row) {
+		if ( err || row == undefined ){			
+			return null;
+		}else{
+			keys.push(row.key);
+		}
+	});
 	cacheDb.close();
 
+	return keys;
+};
+
+exports.getActiveKeys = function(){
+	// query database for all active keys
+	var query = cacheDb.prepare('SELECT keys FROM `cache` WHERE `date_updated` != "" ');
+
+	var keys = [];
+
+	cacheDb.each(query, function(err, row) {
+		if ( err || row == undefined ){			
+			return null;
+		}else{
+			keys.push(row.key);
+		}
+	});
+	cacheDb.close();
+
+	return keys;
 };
 
 exports.putCache = function(key, data){
